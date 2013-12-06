c8bb42415cdf8b7c8b0073e2bda2f6183aad36e2
Alberto Alcaraz <alberto.alcaraz@tangosource.com>
2013-12-06 15:00:15 -0600

    Removed views folder since it would be managed by angular

--- routes/index.js
+++ routes/index.js
@@ -1,9 +1,6 @@
-var applicationController = require('../controllers/application_controller');
 var articlesController    = require('../controllers/articles_controller');
 
 module.exports = function(app){
-  app.get('/', applicationController.index.bind(applicationController));
-
   //Article CRUD
   app.get('/article/new', articlesController.new.bind(articlesController));
   app.post('/article/save', articlesController.save.bind(articlesController));
