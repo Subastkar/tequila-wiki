c38eca8f340aacc44f02ac0d68d2e345d5dbb3a5
Alberto Alcaraz <alberto.alcaraz@tangosource.com>
2013-12-13 14:06:42 -0600

    Listen to connection lost event also save the pending articles whe user get connection back

--- public/angular/app.js
+++ public/angular/app.js
@@ -11,3 +11,30 @@ wikiApp.config(['$routeProvider', function($routeProvider){
     templateUrl: 'angular/views/articles/edit.html'
   });
 }]);
+
+wikiApp.run(function($http, $window, $rootScope) {
+  $rootScope.online = navigator.onLine;
+
+  $window.addEventListener("offline", function(){
+    $rootScope.$apply(function() {
+      $rootScope.online = false;
+    });
+  });
+  $window.addEventListener("online", function(){
+    $rootScope.$apply(function() {
+      $rootScope.online = true;
+      var requests = JSON.parse(localStorage.getItem("create_article"));
+
+      var next = _.after(requests.length, function(){
+        localStorage.removeItem("create_article");
+      });
+
+      _.each(requests, function(article){
+        $http.post('/api/v1/articles/new', {article: article}).success(function(){
+          //Remove item form array instead the whole array
+        });
+        next();
+      });
+    });
+  });
+});
