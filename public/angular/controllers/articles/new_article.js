fbb1b6bbe9eeef52835b374a7bd604fc4acf0770
Alberto Alcaraz <alberto.alcaraz@tangosource.com>
2013-12-13 14:06:02 -0600

    Store new articles on local storage if user offline

--- public/angular/controllers/articles/new_article.js
+++ public/angular/controllers/articles/new_article.js
@@ -1,9 +1,18 @@
-wikiApp.controller('newArticle', [ '$scope', '$http', '$location', function($scope, $http, $location){
+wikiApp.controller('newArticle', [ '$scope', '$http', '$location', '$rootScope', function($scope, $http, $location, $rootScope){
   var onError = function(error){
     alert(error.message);
   };
 
   $scope.saveArticle = function(){
+    if(!$rootScope.online){
+      var requests = localStorage.getItem("create_article") ? JSON.parse(localStorage.getItem("create_article")) : [];
+      requests.push($scope.article);
+
+      localStorage.setItem("create_article", JSON.stringify(requests));
+      alert("This article will be saved in order tu publish it once the connection is restored");
+      $location.path('/');
+      return false;
+    }
     var request = $http.post('/api/v1/articles/new', {article: $scope.article});
 
     request.success(function(data){
